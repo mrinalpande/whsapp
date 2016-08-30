d0b5b043298ca8259d4404be5ea0ffc20e001df4
Mrinal Pande <pande_mrinal@yahoo.com>
2016-08-30 18:15:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+var GAME = new WHS.World({
+stats: 'fps', // fps, ms, mb
+autoresize: true,
+
+gravity: {
+x: 0,
+y: -100,
+z: 0
+},
+
+camera: {
+far: 10000,
+y: 10,
+z: 30
+}
+});
+
+new WHS.Sphere({
+geometry: {
+radius: 3
+},
+
+mass: 10,
+onlyvis: false,
+
+material: {
+color: 0xffffff,
+kind: 'basic'
+},
+
+pos: {
+x: 0,
+y: 100,
+z: 0
+}
+}).addTo(GAME);
+
+new WHS.Plane({
+geometry: {
+width: 250,
+height: 250
+},
+
+mass: 0,
+
+material: {
+color: 0xff0000,
+kind: 'basic'
+},
+
+pos: {
+x: 0,
+y: 0,
+z: 0
+},
+
+rot: {
+x: -Math.PI / 2
+}
+}).addTo(GAME);
+
+GAME.start();
